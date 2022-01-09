8d92b7fd31222c6387c1249f4d3e549ac02d2000
payal_tringapps <payal.b@tringapps.com>
2022-01-09 18:25:31 +0530

    added code

--- app.js
+++
@@ -1,9 +0,0 @@
-async function getData(x){
-    return "hello "+ x;
-}
-
-async function test(){
-   var a=  await getData("world");
-   return getData(a);
-}
-test().then(x=> console.log(x))
--- destructuringArray.js
+++
@@ -1,6 +0,0 @@
-var arr= [1,2,3, 4,5,6,7];
-let [num1, num2,,num4 = 0, ...nums]= arr; //SPECIFIED A DEFAULT VALUE FOR NUM4
-console.log(num1); //1
-console.log(num2); //2
-console.log(num4);//4
-console.log(nums[0])
